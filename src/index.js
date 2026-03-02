d0c0f4b5cbec00f9c1adc80a07df8abe88366252
Matteo Mazzetti <mazzetti.matteo.ita@gmail.com>
2026-03-02 19:58:34 +0100

    fix: optimized code and add middleware for deploy

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@
 const http = require('http');
 const express = require('express');
 const cors = require('cors');
+const path = require('path'); //aggiunto per il deploy
 const { connectDB } = require('./config/db');
 const env = require('./config/env');
 const apiRoutes = require('./routes');
@@ -21,6 +22,15 @@ const server = http.createServer(app);
 app.use(cors());
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
+
+//per deploy
+// Dice a Express di servire i file statici della cartella frontend-marketplace
+app.use('/frontend-marketplace', express.static(path.join(__dirname, '../frontend-marketplace')));
+// Facoltativo: se l'utente va sulla root del sito, rimandalo alla homepage
+app.get('/', (req, res) => {
+    res.redirect('/frontend-marketplace/homepage.html');
+});
+
 
 // API routes
 app.use('/api', apiRoutes);
